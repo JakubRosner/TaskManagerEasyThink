86e404818b8b246f723fd14d358daefdb4bfff8e
pupan007 <jakub.rosner1@gmail.com>
2021-06-03 17:03:46 +0200

    fixed views.py redundant func

--- ToDoList/views.py
+++ ToDoList/views.py
@@ -15,7 +15,6 @@ class HomePageView(generic.ListView):
 class TaskView(generic.ListView):
     template_name = "task.html"
     model = Task
-    ordering = ["-done"]
 
 
 class CreateTaskView(generic.CreateView):
